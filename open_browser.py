60c39031c27e1916d4ad0e5b143059490794cd50
Pedro Pedrosa <pojpedrosa@Pedros-Air.home>
2018-10-19 22:42:00 +0100

    First commit

---
+++ open_browser.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+url = 'http://www.instagram.com/'
+
+# Open URL in new window, raising the window if possible.
+webbrowser.open_new(url)
---
+++ open_browser_fill_form.py
@@ -0,0 +1,6 @@
+import mechanize
+br = mechanize.Browser()
+br.set_all_readonly(False)    # allow everything to be written to
+br.set_handle_robots(False)   # ignore robots
+br.set_handle_refresh(False)  # can sometimes hang without this
+br.addheaders = [('User-agent', 'Firefox')] # [('User-agent', 'Firefox')]
